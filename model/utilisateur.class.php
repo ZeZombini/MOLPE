f89c7b3e039bf85786b369868b8156a2dca8bf24
SuperOuf <le.super.ouf@gmail.com>
2015-11-20 13:15:49 +0100

    Connexion/inscription

--- model/utilisateur.class.php
+++ model/utilisateur.class.php
@@ -20,5 +20,61 @@ class Utilisateur {
     $dao = new DAO();
     $this->contacts = $dao->getContactsFromUserID($this->idUser);
   }
+
+public login($mail,$mdp){
+      if ($mail != NULL && $mdp !=NULL) {
+              try {
+                $mdp = password($mdp);
+                  $temp = $this->db->query("SELECT count(*) FROM Utilisateur WHERE mail=$mail and mdp=$mdp");
+                  if ($temp != FALSE) {
+                    flash('dberror', 'Ce compte n\'existe pas.');
+                    return false;
+                  } else {
+                    flash('connexion', 'Connexion effectuée.');
+                    return true;
+                  }
+
+              } catch (PDOException $e) {
+                
+              }
+      } else {
+        return false;
+      }
+  }
+
+  public logout(){
+
+  }
+
+  public register($mail,$mdp){    
+    if ($mail != NULL && $mdp !=NULL) {
+              try {
+                $mdp = password($mdp);
+                  $temp = $this->db->query("SELECT count(*) FROM Utilisateur WHERE mail=$mail");
+                  if ($temp == FALSE) {
+                    flash('dberror', 'Cette adresse email est déja utilisée.');
+                    return false;
+                  } else {
+                    $q = "INSERT INTO Utilisateur (mail,mdp) VALUES ('$mail','$mdp')";
+                    $r = $this->db->exec($q);
+                    flash('connexion', 'Inscription réussie.');
+                    return true;
+                  }
+              } catch (PDOException $e) {
+                
+              }
+      } else {
+        return false;
+      }
+  }
+
+    private password($mdp){
+      $salt = "9W5Ntleg691h4208tWKz02D40E6XmFU3";
+      $mdp = hash('sha512', $salt . $mdp);
+      return $mdp;
+  }
+
+
+  
 }
  ?>
