aa24a9b21be34b2b7e136b4b15f3c5c862e0c9ef
ZeZombini <zezombini@gmail.com>
2015-11-30 20:31:18 +0100

    Ajout des vues

---
+++ view/afficher_utilisateur_pro.view.php
@@ -0,0 +1,134 @@
+<?php
+	/* L'ensemble de données à remplir */
+	// $data['nomActeur']					= "Not Bad";
+	// $data['zoneGeo']				= "Grenoble (+- 30km)";
+	// $data['style']					= "Rock";
+	// $data['acteur']					= "groupe";
+	// $data['groupePref']				= null;
+	// $data['pourcentageCommission']	= null;
+	// $data['desc']					= "Groupe multiculturel indépendant français formé en 2013.<br>Adèle Christin<br>Alexandre Cleyet-Merle<br>Joris Stocker";
+	// $data['matosDispo'] 			= "Batterie";
+	// $data['telephone']				= "06.02.03.05.09";
+	// $data['site']					= "http://www.youtube.com/channel/notbadrecords";
+	// $data['email'] 					= "notbadrecords38@gmail.com";
+
+
+	
+	// $data['nomActeur']						= "Jean-Edouard de Beaurepaire";
+	// $data['zoneGeo']					= "Grenoble (+- 20km)";
+	// $data['style']					= "Rock, Jazz, Blues";
+	// $data['acteur']					= "booker";
+	// $data['groupePref']				= "Moyen, amateur";
+	// $data['pourcentageCommission']	= "9%";
+	// $data['desc']					= "Booker dynamique en freelance depuis cinq ans sur Grenoble et son agglo, nombreux contacts notamment pour des petites scènes et animation de soirée. Commission négociable.";
+	// $data['telephone']				= "0476010203";
+	// $data['site']				= "www.jedlbooking.fr";
+	// $data['email']				= "je.beaurepaire@gmail.com";
+	
+	
+	
+	// $data['matosDispo'] 				= null;
+	$data['img1']					= null;
+	$data['img2']					= null;
+	$data['img3']					= null;
+	$data['video']					= null; 
+	
+	
+	
+	
+	$data['nomActeur']					= "En avant la musique !";
+	$data['zoneGeo']				= "Voiron (+- 10km)";
+	$data['style']					= null;
+	$data['acteur']					= "orga";
+	$data['groupePref']				= null;
+	$data['pourcentageCommission']	= null;
+	$data['desc']					= "Association musicale proposant régulièrement des concerts sur Voiron, ouverts à tous groupes de tous niveaux et tous styles.";
+	$data['matosDispo'] 			= null;
+	$data['telephone']				= "0476011204";
+	$data['site']					= "ww.eamusique-asso.org";
+	$data['email'] 					= "eamusique-asso@gmail.com";
+	
+	
+	
+	
+	
+	
+	
+	
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Evénementiels </title>
+	 <link rel="stylesheet" href="style/style.css"> 
+  </head>
+  
+  <body>
+   <?php include("header.view.php") ; ?>
+   
+	<div id="content">
+		<div id="fiche-contact">
+			<a href="afficher_utilisateur_public.view.php"><= Voir le profil public</a>
+			<br><br>
+			<img class="img-profil" src="../view/style/img/noimage.jpg"/>
+			<h2><?php echo $data['nomActeur']; ?></h2>
+			
+			<h3>Informations</h3>
+			<hr>
+			<?php
+				if ($data['zoneGeo'] != null)
+					echo "<p>Zone géographique : ".$data['zoneGeo']."</p>\n";
+				if ($data['style'] != null){
+					if ($data['acteur'] == "booker"){
+						echo "<p>Style musical préféré : ".$data['style']."</p>\n";
+					} else {
+						echo "<p>Style musical : ".$data['style']."</p>\n";
+					}
+				}
+				if ($data['groupePref'] != null)
+					echo "<p>Site web : ".$data['groupePref']."</p>\n";
+				if ($data['pourcentageCommission'] != null)
+					echo "<p>Site web : ".$data['pourcentageCommission']."</p>\n";
+				if ($data['desc'] != null)
+					echo "<p>Site web : ".$data['desc']."</p>\n";
+				if ($data['matosDispo'] != null){
+					echo "<p>Matériel disponible pour déplacement :</p>\n<ul>\n";
+					foreach ((array)$data['matosDispo'] as $matos) {
+						echo "    <li>".$matos."</li>\n";
+					}
+					echo "</ul>";
+				}
+				
+				if ($data['matosDispo'] != null)
+					
+				?>
+
+				<h3>Contact</h3>
+				<hr>
+				<?php
+				if ($data['telephone'] != null)
+					echo "<p>Telephone : ".$data['telephone']."</p>\n";
+				if ($data['site'] != null)
+					echo "<p>Site web : <a href=\"".$data['site']."\">".$data['site']."</a></p>\n";
+				if ($data['email'] != null)
+					echo "<p>E-mail : <a href=\"mailto:".$data['email']."\">".$data['email']."</a></p>\n";
+
+				?>
+		</div>
+	</div>
+	
+	<footer>
+	
+	
+	</footer>
+  </body>
+</html>
---
+++ view/afficher_utilisateur_public.view.php
@@ -0,0 +1,143 @@
+<?php
+	/* L'ensemble de données à remplir */
+	
+	// Black Sabath
+	
+	// $data['nomActeur']				= "Black Sabath";
+	// $data['zoneGeo']				= "Les enfers (+- 666km)";
+	// $data['style']					= "Heavy Metal";
+	// $data['acteur']					= "groupe";
+	// $data['groupePref']				= null;
+	// $data['pourcentageCommission']	= null;
+	// $data['desc']					= "Groupe qui fait peur un peu, mais on a Ozzy Osbourne et ça ça claque.";
+	// $data['matosDispo'] 			= "Colombe et chauve-souris";
+	// $data['img1']					= null;
+	// $data['img2']					= null;
+	// $data['img3']					= null;
+	// $data['video']					= "https://www.youtube.com/embed/hkXHsK4AQPs";
+	
+	
+	
+	// NOT BAD 
+	
+	// $data['nomActeur']					= "Not Bad";
+	// $data['zoneGeo']				= "Grenoble (+- 30km)";
+	// $data['style']					= "Rock";
+	// $data['acteur']					= "groupe";
+	// $data['groupePref']				= null;
+	// $data['pourcentageCommission']	= null;
+	// $data['desc']					= "Groupe multiculturel indépendant français formé en 2013.<br>Adèle Christin<br>Bla bla<br>Bla bla bla<br>Bla bla<br>";
+	// $data['matosDispo'] 			= "Batterie";
+	// $data['img1']					= null;
+	// $data['img2']					= null;
+	// $data['img3']					= null;
+	// $data['video']					= null;
+	
+	
+	// $data['nomActeur']						= "Jean-Edouard de Beaurepaire";
+	// $data['zoneGeo']					= "Grenoble (+- 20km)";
+	// $data['style']					= "Rock, Jazz, Blues";
+	// $data['acteur']					= "booker";
+	// $data['groupePref']				= "Moyen, amateur";
+	// $data['pourcentageCommission']	= "9%";
+	// $data['desc']					= "Booker dynamique en freelance depuis cinq ans sur Grenoble et son agglo, nombreux contacts notamment pour des petites scènes et animation de soirée. Commission négociable.";
+	// $data['matosDispo'] 				= null;
+	$data['img1']					= null;
+	$data['img2']					= null;
+	$data['img3']					= null;
+	$data['video']					= null; 
+	
+	
+	
+	$data['nomActeur']					= "En avant la musique !";
+	$data['zoneGeo']				= "Voiron (+- 10km)";
+	$data['style']					= null;
+	$data['acteur']					= "orga";
+	$data['groupePref']				= null;
+	$data['pourcentageCommission']	= null;
+	$data['desc']					= "Association musicale proposant régulièrement des concerts sur Voiron, ouverts à tous groupes de tous niveaux et tous styles.";
+	$data['matosDispo'] 			= null;
+	$data['telephone']				= null;
+	$data['site']					= "ww.eamusique-asso.org";
+	$data['email'] 					= null;
+	
+	
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Evénementiels </title>
+	 <link rel="stylesheet" href="style/style.css"> 
+  </head>
+  
+  <body>
+   <?php include("header.view.php") ; ?>
+   
+	<div id="content">
+		<div id="fiche-contact">
+			<?php if($data['connecte']){echo "<a href=\"afficher_utilisateur_pro.view.php\">Voir le profil pro =></a>";} ?>
+			<br><br>
+			<img class="img-profil" src="../view/style/img/noimage.jpg"/>
+			<h2><?php echo $data['nomActeur']; ?></h2>
+			
+			<h3>Informations</h3>
+			<hr>
+			<?php
+				if ($data['zoneGeo'] != null)
+					echo "<p>Zone géographique : ".$data['zoneGeo']."</p>\n";
+				if ($data['style'] != null){
+					if ($data['acteur'] == "booker"){
+						echo "<p>Style musical préféré : ".$data['style']."</p>\n";
+					} else {
+						echo "<p>Style musical : ".$data['style']."</p>\n";
+					}
+				}
+				if ($data['groupePref'] != null)
+					echo "<p>Groupe préférés : ".$data['groupePref']."</p>\n";
+				if ($data['pourcentageCommission'] != null)
+					echo "<p>Pourcentage de commission : ".$data['pourcentageCommission']."</p>\n";
+				if ($data['desc'] != null)
+					echo "<p>Description : ".$data['desc']."</p>\n";
+				if ($data['matosDispo'] != null){
+					echo "<p>Matériel disponible pour déplacement :</p>\n<ul>\n";
+					foreach ((array)$data['matosDispo'] as $matos) {
+						echo "    <li>".$matos."</li>\n";
+					}
+					echo "</ul>";
+				}
+				
+				if ($data['matosDispo'] != null)
+					
+				?>
+
+				<?php if ($data['acteur'] == "groupe"){
+				echo "<h3>Medias</h3>";
+				echo "<hr>";
+					if ($data['img1'] != null)
+						echo "<img src=\"".$data['img1']."\">";
+					if ($data['img2'] != null)
+						echo "<img src=\"".$data['img2']."\">";
+					if ($data['img3'] != null)
+						echo "<img src=\"".$data['img3']."\">";
+					if($data['video'] != null)
+						echo "<iframe width=\"420\" height=\"315\" src=\"".$data['video']."\" frameborder=\"0\" allowfullscreen></iframe>";
+				}
+					?>
+		</div>
+	</div>
+	
+	<footer>
+	
+	
+	</footer>
+  </body>
+</html>
