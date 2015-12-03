8be713539be2fc1419bb728ad16d5ea032bcc1fd
ZeZombini <zezombini@gmail.com>
2015-12-03 14:01:43 +0100

    Fignollage du grand header

--- view/header.view.php
+++ view/header.view.php
@@ -1,22 +1,35 @@
+<?php
+	$data['connecte'] = true;
+
+?>
+
+
 <header>
 	<div id="header-content">
 		<img id="logo" src="../view/style/img/logo.png" />
 		<div id="header-top">
 			<div id="news">
 				<p>Ouverture de la programmation pour "Musilac"</p>
-				<p>La musique donne une âme à nos coeurs et des ailes à la pensée. - Platon"</p>
+				<p>La musique donne une Ã¢me Ã  nos coeurs et des ailes Ã  la pensÃ©e. - Platon"</p>
 			</div>
-			</div>			
-			<div id="header-bottom">		
-				<nav>
-					<ul>
-						<li><a href="#">Accueil</a></li><!--
-						--><li><a href="#">Rechercher</a></li><!--
-						--><li><a href="#">Mes contacts</a></li><!--
-						--><li><a href="#">Mon compte</a></li>
-					</ul>
-				</nav>
-			</div>
+			<?php 
+				if ($data['connecte'] == true){
+					echo "<div id=\"account\">";
+					echo "</div>";
+				} else {
+					
+				}			
+			?>
+		</div>			
+		<div id="header-bottom">		
+			<nav>
+				<ul>
+					<li><a href="#">Accueil</a></li><!--
+					--><li><a href="#">Rechercher</a></li><!--
+					--><li><a href="#">Mes contacts</a></li><!--
+					--><li><a href="#">Mon compte</a></li>
+				</ul>
+			</nav>
 		</div>
 	</div>
 </header>
