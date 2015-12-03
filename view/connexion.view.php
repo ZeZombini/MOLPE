1b4795e5c7c699e5297edd9b6761605cf5362ef7
Armand0s <armand.souchon@gmail.com>
2015-12-03 15:19:35 +0100

    merge

--- view/connexion.view.php
+++ view/connexion.view.php
@@ -1,4 +1,13 @@
 
+<html>
+  <head>
+    <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Evénementiels </title>
+	 <link rel="stylesheet" href="style/style.css"> 
+  </head>
+  
+  <body>
+   <?php include("header.view.php") ; ?>
+   
 	<div id="content">
 		<div id="content-connexion">
 			<h2>Connexion</h2>
@@ -11,6 +20,9 @@
 
 				<p><a href="#">Mot de passe oublié ?</a></p>
 
+				
+				<p><a href="#">Mot de passe oublié ?</a></p>
+				
 				<input type="submit" value="Submit">
 			</form>
 		</div>
--- view/header.view.php
+++ view/header.view.php
@@ -1,3 +1,9 @@
+<?php
+	$data['connecte'] = true;
+
+?>
+
+
 <header>
 	<div id="header-content">
 		<img id="logo" src="<?= $config['project_path']?>view/style/img/logo.png" />
@@ -5,6 +11,7 @@
 			<div id="news">
 				<p>Ouverture de la programmation pour "Musilac"</p>
 				<p>La musique donne une âme à nos coeurs et des ailes à la pensée. - Platon"</p>
+<<<<<<< HEAD
 			</div>
 			<div id="header-right">
 				<?php
@@ -19,7 +26,33 @@
 					echo "<span><a href=\"" . $config['project_path'] . "connexion/\">Connexion</a> | <a href=\"" . $config['project_path'] . "inscription/\">Inscription</a></span>\n";
 					echo "</div>\n";
 				}?>
+=======
+>>>>>>> origin/master
 			</div>
+			<?php 
+				if ($data['connecte'] == true){
+					echo "<div id=\"account\">";
+					echo "    <div id=\"account-left\">";
+					echo "    <p>Jean-Edouard</p>";
+					echo "	  <p>de Beaurepaire</p>";
+					echo "	  <a href=\"#\">Déconnexion</a>";
+					echo "    </div>";
+					echo "    <img src=\"style/img/noimage.jpg\">";
+					echo "</div>";
+				} else {
+					
+				}			
+			?>
+		</div>			
+		<div id="header-bottom">		
+			<nav>
+				<ul>
+					<li><a href="#">Accueil</a></li><!--
+					--><li><a href="#">Rechercher</a></li><!--
+					--><li><a href="#">Mes contacts</a></li><!--
+					--><li><a href="#">Mon compte</a></li>
+				</ul>
+			</nav>
 		</div>
 		<div id="header-bottom">
 			<nav>
