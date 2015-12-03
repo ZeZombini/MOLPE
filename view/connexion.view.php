782266f2b29507aa53afe49f204303147cb6b1d3
Armand0s <armand.souchon@gmail.com>
2015-12-03 15:20:05 +0100

    ta race

--- view/connexion.view.php
+++ view/connexion.view.php
@@ -1,4 +1,6 @@
 
+<<<<<<< HEAD
+=======
 <html>
   <head>
     <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Evénementiels </title>
@@ -8,6 +10,7 @@
   <body>
    <?php include("header.view.php") ; ?>
    
+>>>>>>> origin/master
 	<div id="content">
 		<div id="content-connexion">
 			<h2>Connexion</h2>
@@ -17,12 +20,15 @@
 				<input type="text" name="email"/>
 				<p>Mot de passe</p>
 				<input type="password" name="mdp"/>
+<<<<<<< HEAD
 
 				<p><a href="#">Mot de passe oublié ?</a></p>
 
+=======
 				
 				<p><a href="#">Mot de passe oublié ?</a></p>
 				
+>>>>>>> origin/master
 				<input type="submit" value="Submit">
 			</form>
 		</div>
