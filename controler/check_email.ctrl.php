74e4f26e1a4fdef64cb6d847df73561046003d47
Armand0s <armand.souchon@gmail.com>
2015-12-18 16:32:51 +0100

    gestion differents cas d'erreur + appel dao verif compte

--- controler/check_email.ctrl.php
+++ controler/check_email.ctrl.php
@@ -5,11 +5,11 @@ if(isset($_GET['email']) && isset($_GET['key'])) {
     $res = $dao->valider_compte($_GET['email'],$_GET['key']);
     if ($res) {
         header("Location : " . $config['project_path'] . "connexion?fail=0");
+    } else {
+        header("Location : " . $config['project_path'] . "connexion?fail=3");
     }
 } else {
     header("Location : " . $config['project_path']);
 }
-
-
 
 ?>
