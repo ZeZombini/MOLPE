c3996b32ea2df646d3efc92a7c3ff9f3fce5943a
ZeZombini <zezombini@gmail.com>
2015-12-18 16:14:06 +0100

    Modif user

--- view/afficher_utilisateur.view.php
+++ view/afficher_utilisateur.view.php
@@ -1,9 +1,9 @@
 <?php
 	/* L'ensemble de donn�es � remplir */
 	$data['nom']					= "Not Bad";
-	$data['zoneGeo']				= "Grenoble (+- 30km)";
+	$data['ville']				= "Grenoble (+- 30km)";
 	$data['style']					= "Rock";
-	$data['acteur']					= "groupe";
+	$data['type']					= "groupe";
 	$data['groupePref']				= null;
 	$data['pourcentageCommission']	= null;
 	$data['desc']					= "Groupe multiculturel ind�pendant fran�ais form� en 2013.<br>Ad�le Christin<br>Bla bla<br>Bla bla bla<br>Bla bla<br>";
@@ -22,37 +22,48 @@
 
 
 
-<!DOCTYPE html>
-
-<html>
-  <head>
-    <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Ev�nementiels </title>
-	 <link rel="stylesheet" href="style/style.css">
-  </head>
-
-  <body>
-   <?php include("header.view.php") ; ?>
-
-	<div id="content">
-		<div id="fiche-contact">
-			<img class="banniere_profil" src="$config['project_path']/view/style/img/">;
-			<div id="fiche-contact-top">
-				<img class="img-profil" src="../view/style/img/noimage.jpg"/>
-				<h3>Lorem Ipsum</h3>
-				<p>Groupe</p>
-				<p>Vous êtes booker de ce groupe</p>
-			</div>
-			<div id="fiche-contact-info">
+<div id="content">
+	<div id="fiche-contact">
+		<img class="banniere_profil" src="$config['project_path']/view/style/img/">;
+		<div id="fiche-contact-top">
+			<img class="img-profil" src="../view/style/img/noimage.jpg"/>
+			<h3>Lorem Ipsum</h3>
+			<p><?=$data['type']?></p>
+			<p>Vous êtes booker de ce groupe</p>
+		</div>
+		<div id="information-contact">
+			<div>
 				<h4>Informations</h4>
 				<hr>
-				<p>Zone géographique : Grenoble (+- 10km)</p>
+			<?php if($data['ville']) : ?>
+				<p>Ville : Grenoble (+- 10km)</p>
+			<?php endif; if($data['style']) : ?>
 				<p>Style musical préféré : Jazz</p>
-				<p>Site web : <a href="#"></a></p>
+			<?php endif; if($data['site']) :?>
+				<p>Site web : <a href="<?=$data['site']?>"><?=$data['site']?></a></p>
+			<?php endif ?>
 			</div>
+
+		<?php if ($data['type'] == "organisateur" && $data['evenement'] != null) : ?>
 			<div>
-
+				<h4>Evènements</h4>
+				<hr>
+				<div class="evenement">
+					<p><?=$data['evenement']['nom']?></p>
+					<p class="data"><?=$data['evenement']['date']?>, à <?=$data['evenement']['ville']?></p>
+					<p>Artiste présent : <?=$data['evenement']['artistes']?></p>
+					<p>Adresse : <?=$data['evenement']['nom']?></p>
+				</div>
 			</div>
+		<?php endif; if ($data['type'] == "organisateur" && $data['scene'] != null) : ?>
 
+
+
+
+		<?php endif; if ($data['type'] == "booker" && $data['groupeGéré'] != null: ?>
+			<?php foreach ($data['groupeGéré'] as $grp) {
+				echo "<img href=\"".$grp['img']"\"/>";
+			} ?>
 
 
 			<div id="fiche-contact-contact">
@@ -63,13 +74,6 @@
 				<p>Email : </p>
 			</div>
 
-
 		</div>
 	</div>
-
-	<footer>
-
-
-	</footer>
-  </body>
-</html>
+</div>
