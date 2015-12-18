6cedadcbc24c4e6fb5825b92842c643a7d877586
ZeZombini <zezombini@gmail.com>
2015-12-18 13:09:04 +0100

    Modif connexion

--- view/connexion.view.php
+++ view/connexion.view.php
@@ -1,17 +1,18 @@
 <div id="content">
-		<div id="content-connexion">
-			<h2>Connexion</h2>
+<h2>Connexion</h2>
+	<div id="content-connexion">
+		// Fail = 1 pas demail
+		// Fail = 2 email pas valide
 
-			<form action="<?= $config['project_path'] ?>controler/check_login.ctrl.php" method="get" onSubmit="">
-				<p>Adresse e-mail :</p>
-				<input type="text" name="email"/>
-				<p>Mot de passe</p>
-				<input type="password" name="mdp"/>
-				<input type="hidden" name="mdp_encoded"/>
-				<p><a href="#">Mot de passe oublié ?</a></p>
-				<input type="submit" value="Submit">
-			</form>
-		</div>
+
+		<form action="<?= $config['project_path'] ?>controler/check_login.ctrl.php" method="get" onSubmit="">
+			<p>Adresse e-mail :</p>
+			<input type="text" name="email" value="<?=$_GET['email'] ?>"/>
+			<p>Mot de passe</p>
+			<input type="password" name="mdp"/>
+			<input type="hidden" name="mdp_encoded"/>
+			<p><a href="#">Mot de passe oublié ?</a></p>
+			<input type="submit" value="Submit">
+		</form>
 	</div>
-</body>
-</html>
+</div>
