8b2157606a03bc7772a28624f39efe284e7972f6
Armand0s <armand.souchon@gmail.com>
2015-12-18 14:34:23 +0100

    Gestion des flag 'fail' pour les messages de retour

---
+++ controler/check_email.ctrl.php
@@ -0,0 +1,15 @@
+<?php
+include("../model/DAO.class.php");
+if(isset($_GET['email']) && isset($_GET['key'])) {
+    $dao = new DAO();
+    $res = $dao->valider_compte($_GET['email'],$_GET['key']);
+    if ($res) {
+        header("Location : " . $config['project_path'] . "connexion?fail=0");
+    }
+} else {
+    header("Location : " . $config['project_path']);
+}
+
+
+
+?>
