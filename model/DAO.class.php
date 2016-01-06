6a7ab78c54e956240e7446c7c6b0b583e6ef5b55
Catka <cetra38@hotmail.fr>
2016-01-06 11:47:39 +0100

    Update DAO.class.php

--- model/DAO.class.php
+++ model/DAO.class.php
@@ -14,8 +14,11 @@
     const R_CLASS = 0;
 // Constructeur ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     function __construct() {
+      echo("ici avant le try");
       try {
+        echo("le try a commencééééé");
         $this->db = new PDO('mysql:host=149.91.81.185;dbname=Molpe', 'molpe', 'esirn');
+        echo("connexion effectuée à la db");
         //$this->db = new PDO("sqlite:../data/molpe.db");
       } catch (PDOException $e) {
         exit("Erreur ouverture BD : ".$e->getMessage());
