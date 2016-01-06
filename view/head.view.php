935b448496c41325279a944bafb1722f57156900
Catka <cetra38@hotmail.fr>
2016-01-06 11:20:35 +0100

    Update head.view.php

--- view/head.view.php
+++ view/head.view.php
@@ -3,11 +3,11 @@
   <head>
     <title>MOLPE - Moteur d'Organisation, Listing et Partage d'Evénementiels </title>
     <meta charset="UTF-8">
-	 <link rel="stylesheet" href="view/style/style.css">
-	 <link rel="stylesheet" href="view/style/mobile.css">
+	 <link rel="stylesheet" href="<?= $config['project_path'] ?>/view/style/style.css">
+	 <link rel="stylesheet" href="<?= $config['project_path'] ?>/view/style/mobile.css">
    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
-   <script type='text/javascript' src='<?= $config['project_path'] ?>data/js/header.js'></script>
-   <script type='text/javascript' src='<?= $config['project_path'] ?>data/js/securepass.js'></script>
+   <script type='text/javascript' src='<?= $config['project_path'] ?>/data/js/header.js'></script>
+   <script type='text/javascript' src='<?= $config['project_path'] ?>/data/js/securepass.js'></script>
   </head>
 
   <body>
