896650e4cb7f53862169b67c84d45970a684d7b3
Catka <cetra38@hotmail.fr>
2016-01-07 15:36:40 +0100

    correction de l'accent
    correction de l'accent qui fait rager les gens qui confondent pas Metallica et Nirvana

--- view/recherche.view.php
+++ view/recherche.view.php
@@ -114,7 +114,7 @@
 					<input type="checkbox" name="style" value="funk"><div>Funk</div>
 					<input type="checkbox" name="style" value="hardrock"><div>Hard Rock</div>
 					<input type="checkbox" name="style" value="jazz"><div>Jazz</div> <br />
-					<input type="checkbox" name="style" value="metal"><div>Métal</div>
+					<input type="checkbox" name="style" value="metal"><div>Metal</div>
 					<input type="checkbox" name="style" value="pop"><div>Pop</div>
 					<input type="checkbox" name="style" value="punk"><div> Punk</div>
 					<input type="checkbox" name="style" value="rnb"><div>R&B</div> <br />
@@ -182,7 +182,7 @@
 					<input type="checkbox" name="style" value="funk"><div>Funk</div>
 					<input type="checkbox" name="style" value="hardrock"><div>Hard Rock</div>
 					<input type="checkbox" name="style" value="jazz"><div>Jazz</div> <br />
-					<input type="checkbox" name="style" value="metal"><div>Métal</div>
+					<input type="checkbox" name="style" value="metal"><div>Metal</div>
 					<input type="checkbox" name="style" value="pop"><div>Pop</div>
 					<input type="checkbox" name="style" value="punk"><div> Punk</div>
 					<input type="checkbox" name="style" value="rnb"><div>R&B</div> <br />
@@ -257,7 +257,7 @@
 					<input type="checkbox" name="style" value="funk"><div>Funk</div>
 					<input type="checkbox" name="style" value="hardrock"><div>Hard Rock</div>
 					<input type="checkbox" name="style" value="jazz"><div>Jazz</div> <br />
-					<input type="checkbox" name="style" value="metal"><div>Métal</div>
+					<input type="checkbox" name="style" value="metal"><div>Metal</div>
 					<input type="checkbox" name="style" value="pop"><div>Pop</div>
 					<input type="checkbox" name="style" value="punk"><div> Punk</div>
 					<input type="checkbox" name="style" value="rnb"><div>R&B</div> <br />
