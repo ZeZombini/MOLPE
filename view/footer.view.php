a2df5734ccf47bdd79b3f6a17f8abd9dbff7ef4b
SuperOuf <le.super.ouf@gmail.com>
2016-01-08 10:58:56 +0100

    footer

--- view/footer.view.php
+++ view/footer.view.php
@@ -1,6 +1,6 @@
 <footer>
 	<ul>
-		<li><img src=\"" . $config['project_path'] . "data/img/minilogo.png">";> Molpe, 2015-2016</li>
+		<li><?php echo "<img src=\"" . $config['project_path'] . "data/img/minilogo.png"\">"; ?> Molpe, 2015-2016</li>
 		<li><a href="#">Conditions générales d'utilisation</a></li>
 		<li><a href="#">Plan du site</a></li>
 		<li><a href="#">A propos</a></li>
