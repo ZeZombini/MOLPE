efb3df6171b401d10d73fa249b123b543d61d4b8
SuperOuf <le.super.ouf@gmail.com>
2016-01-08 11:26:49 +0100

    accueil

---
+++ view/index.view.php
@@ -0,0 +1,19 @@
+<div id="content">
+	<h2>Bienvenue sur Molpé</h2>
+	<div class="33page">
+		<h3>Présentation</h3>
+		<p>Texte présentation</p>
+	</div>
+	<div class="33page">
+		<h3>Truc</h3>
+	</div>
+	<div class="33page">
+		<h3>Connexion/Inscription</h3>
+		<form action="controler/connexion.ctrl.php">
+			<input type="submit" value="Se connecter">
+		</form>
+		<form action="controler/inscription.ctrl.php">
+			<input type="submit" value="S'inscrire">
+		</form>
+	</div>
+</div>
