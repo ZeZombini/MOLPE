6353c5968b09cb0ad663ea7ded342d0a71712041
SuperOuf <le.super.ouf@gmail.com>
2016-01-08 10:54:30 +0100

    footer

--- index.php
+++ index.php
@@ -29,26 +29,31 @@ $app->get("/connexion", function() use ($app,$config) {
     //echo("<h1>Page connexion</h1>");
     insertHead($app,$config);
     include ("controler/connexion.ctrl.php");
+    insertFooter($app,$config);
 });
 
 $app->get("/inscription", function() use ($app,$config) {
     //echo("<h1>Page inscription</h1>");
     insertHead($app,$config);
     include ("controler/inscription.ctrl.php");
+     insertFooter($app,$config);
 });
 
 $app->get("/recherche", function() use ($app,$config) {
     insertHead($app,$config);
     include ("controler/recherche.ctrl.php");
+     insertFooter($app,$config);
 });
 
 $app->get("/user", function () use ($app,$config) {
     insertHead($app,$config);
     include ("controler/afficher_utilisateur.ctrl.php");
+     insertFooter($app,$config);
 });
 
 $app->get("/check_login", function () use ($app,$config) {
     include ("controler/check_login.ctrl.php");
+     insertFooter($app,$config);
 });
 
 
--- view/footer.view.php
+++ view/footer.view.php
@@ -1,9 +1,9 @@
 <footer>
-<ul>
-	<li><img src=\"" . $config['project_path'] . "data/img/minilogo.png">";> Molpe, 2015-2016</li>
-	<li><a href="#">Conditions générales d'utilisation</a></li>
-	<li><a href="#">Plan du site</a></li>
-	<li><a href="#">A propos</a></li>
-	<li><a href="#">Contact</a></li>
-</ul>
+	<ul>
+		<li><img src=\"" . $config['project_path'] . "data/img/minilogo.png">";> Molpe, 2015-2016</li>
+		<li><a href="#">Conditions générales d'utilisation</a></li>
+		<li><a href="#">Plan du site</a></li>
+		<li><a href="#">A propos</a></li>
+		<li><a href="#">Contact</a></li>
+	</ul>
 </footer>
