673f27e098c1b1201e5957cb70610efbe7f03605
SuperOuf <le.super.ouf@gmail.com>
2016-01-08 11:05:32 +0100

    footer

--- view/footer.view.php
+++ view/footer.view.php
@@ -1,9 +1,15 @@
 <footer>
 	<ul>
-		<li><?php echo "<img src='../data/img/minilogo.png'>"; ?> Molpe, 2015-2016</li>
-		<li><a href="#">Conditions générales d'utilisation</a></li>
-		<li><a href="#">Plan du site</a></li>
-		<li><a href="#">A propos</a></li>
-		<li><a href="#">Contact</a></li>
+		<table>
+			<td>
+				<li><?php echo "<img src='../data/img/minilogo.png'>"; ?> Molpe, 2015-2016</li>
+			</td>
+			<td>
+				<li><a href="#">Conditions générales d'utilisation</a></li>
+				<li><a href="#">Plan du site</a></li>
+				<li><a href="#">A propos</a></li>
+				<li><a href="#">Contact</a></li>
+			</td>
+		</table>
 	</ul>
 </footer>
