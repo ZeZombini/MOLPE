de7021f2fefe03b777ec309fc22cf1a339e486f1
Catka <cetra38@hotmail.fr>
2016-01-08 11:50:41 +0100

    Update index.view.php

--- view/index.view.php
+++ view/index.view.php
@@ -2,7 +2,7 @@
 	<h2>Bienvenue sur Molpé</h2>
 	<div class="33page">
 		<h3>Présentation</h3>
-		<p>Molpé - Moteur d’Organisation, Listing et Partage d’événementiel - est un gestionnaire destiné aux acteurs du monde de la musique ; bookers, musiciens et organisateurs d'évènement musicaux.</p>
+		<p>Molpé - Moteur d’Organisation, Listing et Partage d’événementiel - est un gestionnaire destiné aux acteurs du monde de la musique ; bookers, musiciens et organisateurs d'évènement musicaux.<br /> Il permet à ces acteurs de centraliser leur carnet d'adresse, de communiquer entre eux et de partager leurs évènements.</p>
 	</div>
 	<div class="33page">
 		<h3>Connexion/Inscription</h3>
